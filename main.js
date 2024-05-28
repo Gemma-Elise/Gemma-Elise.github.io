d81af769457145ca9fb860c52f57edb55b44bc1b
James Fawcus-Robinson <jameslmfr@gmail.com>
2024-05-28 17:25:38 +0100

    Add the "G flower" icon to the nav bar
    The icon is clickable and brings up the nav menu on screens < 1000px

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+const g_flower = document.querySelector(".g-flower");
 const burger = document.querySelector(".burger-menu");
 const burger_checkbox = document.querySelector(".burger-menu > input");
 const sidebar = document.querySelector(".sidebar");
@@ -13,5 +14,9 @@ function toggle_burger() {
     burger_checkbox.checked = false;
   }
 }
+
+g_flower?.addEventListener("click", () => {
+  if (document.documentElement.clientWidth < 1000) toggle_burger();
+});
 
 burger?.addEventListener("change", () => toggle_burger());
